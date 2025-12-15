4edbb9095bc88cde19706974940ad48daddd3bdc
ehima <ehimareatga063@gmail.com>
2025-12-15 17:07:08 +0000

    User.cs was created to enable the username and eamil also secruity protection for when the user is logged in.

--- AnimeWeb/Models/User.cs
+++ AnimeWeb/Models/User.cs
@@ -2,5 +2,14 @@
 
 public class User
 {
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
     
+    // Store hashed password only
+    public string PasswordHash { get; set; } = null!;
+
+    // Navigation property
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
 }
