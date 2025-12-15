b421fa15bb8128df994a8bbe10891d203903bfb7
ehima <ehimareatga063@gmail.com>
2025-12-15 17:15:12 +0000

    DbContext used to link the database options using sqlite which i found to be the easiet and quickest for this project.

---
+++ AnimeWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AnimeWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Anime> Anime { get; set; } = null!;
+    public DbSet<Review> Reviews { get; set; } = null!;
+}
