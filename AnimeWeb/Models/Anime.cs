07e9073e5df03a31351b11841a96004d7949c5cb
ehima <ehimareatga063@gmail.com>
2025-12-15 17:02:43 +0000

    Added the anime variables for the anime.cs and put the get and set for the this and navigation property added for the review.cs

--- AnimeWeb/Models/Anime.cs
+++ AnimeWeb/Models/Anime.cs
@@ -2,5 +2,13 @@
 
 public class Anime
 {
-    
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+    public string Genre { get; set; } = null!;
+    public int ReleaseYear { get; set; }
+    public string? Description { get; set; }
+
+    // Navigation property
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
 }
