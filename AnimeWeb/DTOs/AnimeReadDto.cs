8ac7d261b40d655bb10c811f3a6c0fbe8e4dab7f
ehima <ehimareatga063@gmail.com>
2025-12-16 20:02:15 +0000

    Created this animereaddto for the api that will read and db infromation giving by genre titkle etc...

---
+++ AnimeWeb/DTOs/AnimeReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimeWeb.DTOs;
+
+public class AnimeReadDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public int ReleaseYear { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
