67747f52e274369c6f9e8678f9ba1a91b61b077f
ehima <ehimareatga063@gmail.com>
2025-12-16 20:03:21 +0000

    This is the animecreatedto that creates the stable to structure the json api of anime.web

---
+++ AnimeWeb/DTOs/AnimeCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimeWeb.DTOs;
+
+public class AnimeCreateDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public int ReleaseYear { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
